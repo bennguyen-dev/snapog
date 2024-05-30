1f84a0c29cc9f1f7be6352cba07c0614af9e7051
Ben Nguyen <bennguyen.dev.vn@gmail.com>
2024-05-30 11:29:28 +0700

    feat: update .middeware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,3 @@ export { auth as middleware } from "@/auth";
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
-
-export const runtime = "experimental-edge";
