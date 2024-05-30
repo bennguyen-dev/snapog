eaf43f8d6a93673c079a8f2159383b7fb6734026
Ben Nguyen <bennguyen.dev.vn@gmail.com>
2024-05-30 17:26:55 +0700

    feat: remove middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,3 @@
-import { auth } from "@/auth";
-import { ROUTES } from "@/lib/constants";
-import { NextRequest, NextResponse } from "next/server";
-
-export const runtime = "nodejs";
-
-export async function middleware(request: NextRequest) {
-  const checkRouteAuth = ROUTES.some((route) => {
-    if (request.nextUrl.pathname === route.path && route.auth) {
-      return true;
-    }
-  });
-
-  const session = await auth();
-
-  if (!session?.user && checkRouteAuth) {
-    const newUrl = new URL("/signin", request.nextUrl.origin);
-    return NextResponse.redirect(newUrl);
-  }
-}
-
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
