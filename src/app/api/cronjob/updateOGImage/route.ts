d9b03edcd60927e302de0d149d4e30a426cb9d82
Ben Nguyen <bennguyen.dev.vn@gmail.com>
2024-06-17 11:04:22 +0700

    feat: update config crons vercel

--- src/app/api/cronjob/updateOGImage/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import cronJobService from "@/sevices/cronJob/cronJob.service";
-
-export async function GET() {
-  const result = await cronJobService.updateOGImage();
-  return NextResponse.json(result, { status: result.status });
-}
