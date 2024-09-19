d88bba6906f08e43c8a7940e19bc324d9de74365
Ben Nguyen <bennguyen.dev.vn@gmail.com>
2024-09-19 18:23:06 +0700

    feat: init lemonsqueezy and update layout dashboard

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 export interface IResponse<T> {
   message: string;
   status: number;
@@ -5,3 +7,13 @@ export interface IResponse<T> {
 }
 
 export type ValuesOf<T> = T[keyof T];
+
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: ReactNode;
+  label?: string;
+  description?: string;
+}
