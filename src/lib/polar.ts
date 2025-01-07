c31828f66c5263badcbf237e5470270b1a84c1e5
Ben Nguyen <bennguyen.dev.vn@gmail.com>
2025-01-07 17:24:00 +0700

    chore: add log

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -1,6 +1,17 @@
 import { Polar } from "@polar-sh/sdk";
 
+console.log(
+  "a 😋",
+  {
+    env: process.env.NODE_ENV,
+    b: process.env.VERCEL_ENV,
+    c: process.env.VERCEL_URL,
+    d: process.env.POLAR_ACCESS_TOKEN,
+  },
+  "",
+);
+
 export const polarApi = new Polar({
   accessToken: process.env.POLAR_ACCESS_TOKEN!,
-  server: process.env.NODE_ENV === "production" ? "production" : "sandbox",
+  server: process.env.VERCEL_ENV === "production" ? "production" : "sandbox",
 });
